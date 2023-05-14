8ec8b97aaee445a3b53de2a228ebdfd929a00a37
NazmiHashim <93772662+NazmiHashim@users.noreply.github.com>
2023-05-14 16:39:07 +0800

    Pass between 2 array

--- radix.java
+++ radix.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class RadixSort {
 
     // Using counting sort to sort the elements in the basis of significant places
-    void countingSort(int array[], int size, int place) {
+    void countingSort(int array[], int list[], int size, int place) {
         int[] output = new int[size];
 
         //int[] count = new int[max + 1];
@@ -23,13 +23,13 @@ class RadixSort {
         // Place the elements in sorted order
         for (int i = size - 1; i >= 0; i--) {
             output[count[(array[i] / place) % 10] - 1] = array[i];
-            System.out.println(Arrays.toString(output));
+            //System.out.println(Arrays.toString(output));
             count[(array[i] / place) % 10]--;
         }
 
 
         for (int i = 0; i < size; i++)
-            array[i] = output[i];
+            list[i] = output[i];
     }
 
     // Function to get the largest element from an array
@@ -46,22 +46,44 @@ class RadixSort {
         // Get maximum element
         int max = getMax(array, size);
 
+        int[] pass1 = new int[size];
+        int[] pass2 = new int[size];
+
+        // Using System.arraycopy() method to copy array elements
+        System.arraycopy(array, 0, pass1, 0, size);
+
+        boolean checkEven = false;
+
         // Apply counting sort to sort elements based on place value.
+        // if it is 1st,3rd,5th... time to sort,arr1 is involved
+        // if it is 2nd,4th, 6th... time to sort, arr2 is involved
         for (int place = 1; max / place > 0; place *= 10) {
-            countingSort(array, size, place);
-            System.out.println(Arrays.toString(array));
+            if(!checkEven){
+                countingSort(pass1, pass2, size, place);
+                checkEven = true;
+            } else{
+                    countingSort(pass2, pass1, size, place);
+                    checkEven = false;
+            }
+            //System.out.println(Arrays.toString(array));
         }
+        if(!checkEven)
+            System.arraycopy(pass1, 0, array, 0, size);
+        else
+            System.arraycopy(pass2, 0, array, 0, size);
+
+
     }
 
     // Driver code
     public static void main(String args[]) {
-        int[] data = { 275, 87, 426, 61, 409, 170, 677, 503,  };
+        int[] data = { 275, 87, 426, 61, 409, 170, 677, 503};
         //int[] data = { 9,5,7,12,2 };
         int size = data.length;
-        System.out.println("Array Before Sorted: \n" + Arrays.toString(data));
         RadixSort rs = new RadixSort();
         rs.radixSort(data, size);
         System.out.println("Sorted Array in Ascending Order: ");
         System.out.println(Arrays.toString(data));
     }
 }
+
